1173299fb66fc98f7a0fccdfc948a74ff7c32c90
Cory Zue <czue@dimagi.com>
2020-04-06 09:18:40 +0200

    add initial functional export tool runner

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import subprocess
+
+import os
+
 import settings
 
 
 def main():
-    print('it works!')
-    print(settings.PROJECT)
-    print(settings.USERNAME)
-    print(settings.AUTH_MODE)
-    print(settings.PASSWORD)
+    base_command = [
+        'commcare-export',
+        '--project', settings.PROJECT,
+        '--username', settings.USERNAME,
+        '--auth-mode', settings.AUTH_MODE,
+        '--password', settings.PASSWORD,
+        '--output-format', settings.OUTPUT_FORMAT,
+    ]
+    filenames = ['leadership_governance.xlsx']
+    for det_file in filenames:
+        det_file_path = os.path.join('det_configs', det_file)
+        command = base_command + ['--query', det_file_path]
+        print(f"running {' '.join(command)}")
+        subprocess.run(command)
 
 
 main()
